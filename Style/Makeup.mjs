5751a7e52e13eaf7cfa96e7b08ff8cbd70f04164
iosef-neeme <iosef.nee@stud.umfcd.ro>
2021-11-27 02:50:20 +0200

    I created a directory, named Style, that contains 2 files, Makeup.css and Makeup.mjs. The files can be use to the folowing:  - easyly modify the default layout of Sugarcube2  - create a lightboxe for pictures in the game  - create a layout template for dialog boxes

---
+++ Style/Makeup.mjs
@@ -0,0 +1,46 @@
+const lightbox = document.createElement("div");
+const image = document.createElement("img");
+/////
+/////
+/////
+function Detect_Click(e)
+{
+    if (e.target != e.currentTarget)
+    {
+        return
+    }
+    e.currentTarget.classList.toggle("active");
+    document.getElementById(image.id).src = "";
+}
+function Detect_Click_On_Picture(e)
+{
+    document.getElementById(lightbox.id).classList.toggle("active");
+    document.getElementById(image.id).src = e.currentTarget.src;
+}
+function Lightbox()
+{ 
+    lightbox.id = "lightbox";
+    document.body.appendChild(lightbox);
+    document.getElementById("lightbox").addEventListener("click", Detect_Click)
+}
+function Lightbox_Image()
+{
+    image.id = "lightbox_image";
+    document.getElementById("lightbox").appendChild(image);
+    const class_name = "lightbox_image";
+    let list = document.getElementsByClassName(class_name);
+    for (let element of list) {element.addEventListener('click', Detect_Click_On_Picture)};
+}
+/////
+/////
+/////
+
+window.onload = function(){
+    if(document.getElementById("init-screen").style.display = "none")
+    {setTimeout(function(){
+        Lightbox();
+        Lightbox_Image();
+        document.getElementById("passages").style.margin = "0px 0px 0px 240px";
+        document.getElementById("passages").style.maxWidth = "65em";
+        }, 0); };
+}

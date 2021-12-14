9f7c77ead7a8597b7d8273614efd1487d658cd0a
iosef-neeme <iosef.nee@stud.umfcd.ro>
2021-12-14 15:58:16 +0200

    I added comments for the functions

--- Style/Makeup.mjs
+++ Style/Makeup.mjs
@@ -12,11 +12,17 @@ function Detect_Click(e)
     e.currentTarget.classList.toggle("active");
     document.getElementById(image.id).src = "";
 }
+//
+//
+//This is the function called by the click event listener atached to images of class "lightbox_image"
 function Detect_Click_On_Picture(e)
 {
     document.getElementById(lightbox.id).classList.toggle("active");
     document.getElementById(image.id).src = e.currentTarget.src;
 }
+//
+//
+//This function creates a div named "lightbox" in which the pictures with the images of class "lightbox_image" will be displayed when they are clicked
 function Lightbox()
 { 
     lightbox.id = "lightbox";
@@ -24,6 +30,9 @@ function Lightbox()
     document.getElementById("lightbox").addEventListener("click", Detect_Click)
     Lightbox_Image();
 }
+//
+//
+//This function creates adds a click event listener to all images of class "lightbox_image"
 function Lightbox_Image()
 {
     image.id = "lightbox_image";
